36654eef5ff73c0f6ebc1c121a817eed72e41fdd
yugykim <82011274+yugykim@users.noreply.github.com>
2022-01-19 14:23:24 -0700

    lint

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/nasa-search-vue/'
-  : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/nasa-search-vue/' : '/',
 }
