1c510ee11a771296cc681bcf01067f2de2525fb9
yugykim <82011274+yugykim@users.noreply.github.com>
2022-01-19 19:22:22 -0700

    created loading page

---
+++ src/components/loading.vue
@@ -0,0 +1,98 @@
+<template>
+<div class="loading">
+  <div class="loader">
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.loading {
+  align-items: center;
+  background-color: #1D1F20;
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+}
+
+.loader  {
+  animation: rotate 1s infinite;  
+  height: 50px;
+  width: 50px;
+}
+
+.loader:before,
+.loader:after {   
+  border-radius: 50%;
+  content: '';
+  display: block;
+  height: 20px;  
+  width: 20px;
+}
+.loader:before {
+  animation: ball1 1s infinite;  
+  background-color: #FF10F0;
+  box-shadow: 30px 0 0 #1F51FF;
+  margin-bottom: 10px;
+}
+.loader:after {
+  animation: ball2 1s infinite; 
+  background-color: white;
+  box-shadow: 30px 0 0 white;
+}
+
+@keyframes rotate {
+  0% { 
+    -webkit-transform: rotate(0deg) scale(0.8); 
+    -moz-transform: rotate(0deg) scale(0.8);
+  }
+  50% { 
+    -webkit-transform: rotate(360deg) scale(1.2); 
+    -moz-transform: rotate(360deg) scale(1.2);
+  }
+  100% { 
+    -webkit-transform: rotate(720deg) scale(0.8); 
+    -moz-transform: rotate(720deg) scale(0.8);
+  }
+}
+
+@keyframes ball1 {
+  0% {
+    box-shadow: 30px 0 0 #1F51FF;
+  }
+  50% {
+    box-shadow: 0 0 0 #1F51FF;
+    margin-bottom: 0;
+    -webkit-transform: translate(15px,15px);
+    -moz-transform: translate(15px, 15px);
+  }
+  100% {
+    box-shadow: 30px 0 0 #1F51FF;
+    margin-bottom: 10px;
+  }
+}
+
+@keyframes ball2 {
+  0% {
+    box-shadow: 30px 0 0 #0FFF50;
+  }
+  50% {
+    box-shadow: 0 0 0 #0FFF50;
+    margin-top: -20px;
+    -webkit-transform: translate(15px,15px);
+    -moz-transform: translate(15px, 15px);
+  }
+  100% {
+    box-shadow: 30px 0 0 #0FFF50;
+    margin-top: 0;
+  }
+}
+
+
+</style>
